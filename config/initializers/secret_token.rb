bbb54b03101a7a0543cf96c33d7608f61b814a03
Brian Watson <brian.a.watson@gmail.com>
2014-02-28 18:42:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Slashmusic::Application.config.secret_key_base = 'efacd9fecc1c3caaebf31f76706b8d317294a50208a65c772324cf8a371b4b533a0635a91bac5f01edc0dd5a7d36b88af595682b7c7fb818d8b40fa24a7cf8db'
